11761ea456be2c384520f2742425a0c157ac546f
Alex.Yuryev <AleksejYurjev@gmail.com>
2023-04-21 15:21:52 +0300

    added carousel

---
+++ js/carousel.js
@@ -0,0 +1,43 @@
+const carousel = document.querySelector(".js-carousel");
+const buttons = document.querySelectorAll(".js-btn");
+firstItem = carousel.querySelectorAll("li")[0];
+
+let isDragStart = false,
+  prevPageX,
+  prevScrollLeft;
+
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX || e.touched[0].pageX;
+  prevScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (!isDragStart) return;
+  e.preventDefault();
+  carousel.classList.add("dragging");
+  let positionDiff = (e.pageX || e.touched[0].pageX) - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+};
+
+const dragStop = () => {
+  isDragStart = false;
+  carousel.classList.remove("dragging");
+};
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("touchend", dragStop);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let firstItemWidth = firstItem.clientWidth + 20;
+    carousel.scrollLeft +=
+      button.id == "prev" ? -firstItemWidth : firstItemWidth;
+  });
+});
